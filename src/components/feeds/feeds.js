8a847ba841e6411c06bd6feb1fa44745adc223ee
AnhVu23 <vu.haianh291@gmail.com>
2018-08-26 22:21:17 +0300

    Improving UI

--- src/components/feeds/feeds.js
+++ src/components/feeds/feeds.js
@@ -9,12 +9,11 @@ class Feeds extends Component {
     return (
       <div className={classes.Container}>
         <Row>
-          {/*<Col xs={0} md={1} lg={2}></Col>*/}
           <Col xs={24} md={{span:22, offset: 1}} lg={{span: 12, offset: 2}}>
-            <Posts/>
+            <Posts className={classes.Post}/>
           </Col>
-          <Col xs={0} lg={6} push={2}>
-            <Card title='Create a new post' bordered={false}>New Post</Card>
+          <Col xs={0} lg={6} push={1}>
+            <Card title='Create a new post' bordered={false} className={classes.Card}>New Post</Card>
           </Col>
         </Row>
       </div>
--- src/components/feeds/posts/post/post.js
+++ src/components/feeds/posts/post/post.js
@@ -11,7 +11,8 @@ const post = (props) => {
   );
   return (
     <List.Item key={props.title}
-               actions={[<IconText type='like-o' text={props.likes}/>, <IconText type='message' text={props.comments}/>]} className={classes.Post}>
+               actions={[<IconText type='like-o' text={props.likes}/>, <IconText type='message' text={props.comments}/>]}
+               className={classes.Post}>
       <List.Item.Meta  title={<a href={props.href}>{props.title}</a>}
                        description={props.username}/>
     </List.Item>
